30b2c5b5a5d2bd0d62788b1af3f70ed4ffe1082f
Samuel Tetteh <khobbylynx55@gmail.com>
2023-11-25 02:09:01 +0000

    Fall back spinner

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 
-import App from './App.jsx'
+import Spinner from './components/spinner/FallbackSpinner'
+
 import '../src/scss/style.scss'
 import './index.css'
 import './slick/slick.css'
 import './slick/slick-theme.css'
 import store from './store/index.js'
 
+// ** Lazy load app
+const LazyApp = lazy(() => import('./App.jsx'))
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-   <React.StrictMode>
-      <Provider store={store}>
-         <App />
-         <ToastContainer
-            position='top-right'
-            autoClose={2000}
-            hideProgressBar={false}
-            newestOnTop
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss={false}
-            draggable={false}
-            pauseOnHover
-            theme='light'
-         />
-      </Provider>
-   </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <Suspense fallback={<Spinner />}>
+        <LazyApp />
+        <ToastContainer
+          position='top-right'
+          autoClose={2000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable={false}
+          pauseOnHover
+          theme='light'
+        />
+      </Suspense>
+    </Provider>
+  </React.StrictMode>
 )
